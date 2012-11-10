27428cf7d7ebb677cbff406dc3027e6309bffae9
Paolo Boschini <boschini.paolo@gmail.com>
2012-11-10 18:58:59 +0100

    commented and refactored

--- Clock.java
+++ Clock.java
@@ -1,7 +1,7 @@
 /**
-* Paints an analog clock synchronized with the system clock.
-* @author Paolo Boschini
-*/
+ * Paints an analog clock synchronized with the system clock.
+ * @author Paolo Boschini
+ */
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -18,11 +18,12 @@ import javax.swing.JPanel;
 class Clock extends JFrame {
 
   private static final long serialVersionUID = 1L;
+  private static final Color BACKGROUND_COLOR = new Color(24, 116, 205); 
 
   public Clock() {
     ClockPanel container = new ClockPanel();
     add(container, BorderLayout.CENTER);
-    setBackground(new Color(24, 116, 205));
+    setBackground(BACKGROUND_COLOR);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     setResizable(false);
     // setSize(500, 500);
@@ -40,26 +41,52 @@ class ClockPanel extends JPanel implements Runnable {
 
   private static final long serialVersionUID = 1L;
   Thread t = new Thread(this);
-  private int thick = 1;
-  int xsec, ysec, xmin, ymin, xhour, yhour;
+
+  /** The coordinates used to paint the clock hands. */
+  int xHandSec, yHandSec, xHandMin, yHandMin, xHandHour, yHandHour;
+
+  /** The size of the clock. */
+  private final int HORIZONTAL_SIZE = 500;
+  private final int VERTICAL_SIZE = 500;
+
+  /** The length of the clock hands relative to the clock size. */
+  private final int secondHandLength = HORIZONTAL_SIZE / 2 - 50;
+  private final int minuteHandLength = HORIZONTAL_SIZE / 2 - 70;
+  private final int hourHandLength = HORIZONTAL_SIZE / 2 - 100;
+
+  /** The distance of the dots from the origin (center of the clock). */
+  private final int DISTANCE_DOT_FROM_ORIGIN = HORIZONTAL_SIZE / 2 - 40;
+
+  private final int DIAMETER_BIG_DOT = 8;
+  private final int DIAMETER_SMALL_DOT = 4;
+  
+  private final static Color GREY_COLOR = new Color(160,160,160);
 
   public ClockPanel() {
-    setMinimumSize(new Dimension(500, 500));
-    setMaximumSize(new Dimension(500, 500));
-    setPreferredSize(new Dimension(500, 500));
+    setMinimumSize(new Dimension(HORIZONTAL_SIZE, VERTICAL_SIZE));
+    setMaximumSize(new Dimension(HORIZONTAL_SIZE, VERTICAL_SIZE));
+    setPreferredSize(new Dimension(HORIZONTAL_SIZE, VERTICAL_SIZE));
     setLayout(null);
     t.start();
   }
 
-  public void run() {
-    while(true) {
-      try {
-        xsec = minToLocation(Calendar.getInstance().get(Calendar.SECOND), 200).x;
-        ysec = minToLocation(Calendar.getInstance().get(Calendar.SECOND), 200).y;
-        xmin = minToLocation(Calendar.getInstance().get(Calendar.MINUTE), 180).x;
-        ymin = minToLocation(Calendar.getInstance().get(Calendar.MINUTE), 180).y;
-        xhour = minToLocation(Calendar.getInstance().get(Calendar.HOUR)*5+getRelativeHour(Calendar.getInstance().get(Calendar.MINUTE)), 150).x;
-        yhour = minToLocation(Calendar.getInstance().get(Calendar.HOUR)*5+getRelativeHour(Calendar.getInstance().get(Calendar.MINUTE)), 150).y;
+  /**
+   * At each iteration we recalculate the coordinates of the clock hands,
+   * and repaint everything.
+   */
+  public void run(){
+    while(true){
+      try{
+        int currentSecond = Calendar.getInstance().get(Calendar.SECOND);
+        int currentMinute = Calendar.getInstance().get(Calendar.MINUTE);
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR);
+
+        xHandSec = minToLocation(currentSecond, secondHandLength).x;
+        yHandSec = minToLocation(currentSecond, secondHandLength).y;
+        xHandMin = minToLocation(currentMinute, minuteHandLength).x;
+        yHandMin = minToLocation(currentMinute, minuteHandLength).y;
+        xHandHour = minToLocation(currentHour * 5 + getRelativeHour(currentMinute), hourHandLength).x;
+        yHandHour = minToLocation(currentHour * 5 + getRelativeHour(currentMinute), hourHandLength).y;
         repaint();
         Thread.sleep(500);
       } catch(InterruptedException ie){
@@ -68,46 +95,65 @@ class ClockPanel extends JPanel implements Runnable {
     }
   }
 
-  public int getRelativeHour(int min) {
+
+  /**
+   * Returns how much the hour hand should be ahead
+   * according to the minutes value.
+   * 04:00, return 0.
+   * 04:12, return 1, so that we move the hour handle ahead of one dot. 
+   * @param min The current minute.
+   * @return The relative offset to add to the hour hand. 
+   */
+  private int getRelativeHour(int min) {
     return min / 12;
   }
 
-  protected void paintComponent(Graphics g) {
+  protected void paintComponent(Graphics g){
     Graphics2D g2 = (Graphics2D)g;
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-      RenderingHints.VALUE_ANTIALIAS_ON);
+        RenderingHints.VALUE_ANTIALIAS_ON);
 
     g2.clearRect(0, 0, getWidth(), getHeight());
 
-    g2.setColor(new Color(200,200,200));
+    // Draw the dots
+    g2.setColor(GREY_COLOR);
     for (int i = 0; i < 60; i++) {
-      g2.setColor((i <= Calendar.getInstance().get(Calendar.SECOND)) ? Color.white : new Color(160,160,160));
-      if(i % 5 == 0)
-        g2.fillOval(minToLocation(i, 210).x - 4 , minToLocation(i, 210).y - 4, 8, 8);
-      else
-        g2.fillOval(minToLocation(i, 210).x - 2 , minToLocation(i, 210).y - 2, 4, 4);
+      
+      Point dotCoordinates = minToLocation(i, DISTANCE_DOT_FROM_ORIGIN);
+      g2.setColor((i <= Calendar.getInstance().get(Calendar.SECOND)) ? Color.white : GREY_COLOR);
+      
+      if (i % 5 == 0) {
+        // big dot
+        g2.fillOval(dotCoordinates.x - (DIAMETER_BIG_DOT / 2),
+              dotCoordinates.y - (DIAMETER_BIG_DOT / 2),
+              DIAMETER_BIG_DOT,
+              DIAMETER_BIG_DOT);
+      } else {
+        // small dot
+        g2.fillOval(dotCoordinates.x - (DIAMETER_SMALL_DOT / 2),
+              dotCoordinates.y - (DIAMETER_SMALL_DOT / 2),
+              DIAMETER_SMALL_DOT,
+              DIAMETER_SMALL_DOT);
+      }
     }
 
+    // Draw the clock hands
     g2.setColor(Color.white);
-    g2.drawString(Calendar.getInstance().get(Calendar.HOUR)+" : ", 430, 485);
-    g2.drawString(Calendar.getInstance().get(Calendar.MINUTE)+" : ", 450, 485);
-    g2.drawString(Calendar.getInstance().get(Calendar.SECOND)+"", 480, 485);
-
-    for (int z = 0; z < thick; z++) {
-      g2.setColor(Color.white);
-      g2.drawOval(25 + z, 25 + z, 450 - (z*2), 450 - (z*2));
-    }
-
-    g2.drawLine(250, 250, xsec, ysec);
-    g2.drawLine(250, 250, xmin, ymin);
-    g2.drawLine(250, 250, xhour, yhour);
+    g2.drawLine(HORIZONTAL_SIZE / 2, VERTICAL_SIZE / 2, xHandSec, yHandSec);
+    g2.drawLine(HORIZONTAL_SIZE / 2, VERTICAL_SIZE / 2, xHandMin, yHandMin);
+    g2.drawLine(HORIZONTAL_SIZE / 2, VERTICAL_SIZE / 2, xHandHour, yHandHour);
   }
 
-  public Point minToLocation(int min, int r) {
-    double t = 2 * Math.PI * (min-15) / 60;
-    int x = (int) Math.round(250 + r * Math.cos(t));
-    int y = (int) Math.round(250 + r * Math.sin(t));
-    // System.out.println(x + ", " + y);
+  /**
+   * Converts current second/minute/hour to x and y coordinates.
+   * @param min The current minute
+   * @param radius The radius length    
+   * @return the coordinates point
+   */
+  private Point minToLocation(int timeStep, int radius) {
+    double t = 2 * Math.PI * (timeStep-15) / 60;
+    int x = (int)(HORIZONTAL_SIZE / 2 + radius * Math.cos(t));
+    int y = (int)(VERTICAL_SIZE / 2 + radius * Math.sin(t));
     return new Point(x, y);
-  }
+  }    
 }
